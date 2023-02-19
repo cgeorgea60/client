9685b47115ca645ac61465b10c957e09b5069977
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-02-19 03:04:12 +0100

    baseURL correction3

--- src/api/RestaurantFinder.js
+++ src/api/RestaurantFinder.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
- 
-  baseURL: "https://restaurants-api.up.railway.app",
- 
+  baseURL: "https://restaurants-api.up.railway.app/api/v1/restaurants",
 });
